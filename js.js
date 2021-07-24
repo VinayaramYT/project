648fa02275a389fb2427c1edde365c32a17d4006
VinayaramYT <86592483+VinayaramYT@users.noreply.github.com>
2021-07-24 19:23:07 +0530

    Add files via upload

--- js.js
+++ js.js
@@ -3,3 +3,6 @@ function myfunction(){
     document.getElementById("demo").innerHTML = "VR INFO TECH TELUGU";
  }
  alert("THIS PAGE IS SAFE");
+
+  var uid = prompt ("Please Enter Your Name:","");
+  document.write("Hello " + uid+"!");
